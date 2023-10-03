de1227ebfba0c367fb88e273c97eebf0bc55a235
Katarzyna Bogusz <kabogusz@interia.pl>
2023-10-03 23:33:06 +0200

    Add changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 'use strict';
 
-function titleClickHandler(event){
+
+function titleClickHandler(event) {
+const clickedElement = this;
  event.preventDefault();
   console.log('Link was clicked!');
-  const clickedElement = this;
-}
+  
+
   /* remove class 'active' from all article links  */
  
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -14,19 +16,25 @@ for(let activeLink of activeLinks){
 }
 
   /* add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post a.active');
+  const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+    activeArticle.classList.remove('active');}
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector:', articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
